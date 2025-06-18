6d10577032461aaa3e26cdcdb409a5ea96c5c6f9
bonbonkeai <168202332+bonbonkeai@users.noreply.github.com>
2025-06-18 13:33:56 +0200

    jingyi_redi

--- srcs/executor/exec_simple.c
+++ srcs/executor/exec_simple.c
@@ -75,20 +75,18 @@ int	exec_simple(t_cmd *cmd, t_shell *shell)
 		print_cmd_error("", "command not found");
 		return (127);
 	}
-
 	if (is_directory(cmd->cmd))
 	{
 		print_cmd_error(cmd->cmd, "Is a directory");
 		return (126);
 	}
-
 	if (if_cmd_builtin(cmd) == 1)
 		return (exec_builtin_main(cmd, shell));
-
 	status = 0;
 	pid = fork();
 	if (pid == 0)
 	{
+		// apply_red(cmd, shell->env);
 		status = execve_bin(cmd, shell->env);
 		exit(status);
 		// exec_simple_exit(cmd);
@@ -106,60 +104,76 @@ int	exec_simple(t_cmd *cmd, t_shell *shell)
 	return (status);
 }
 
+// int	execve_bin(t_cmd *cmd, t_env *env)
+// {
+// 	char	*path;
+// 	char	**arg;
+// 	char	**vars;
+
+// 	path = get_path(cmd, env);
+// 	arg = get_args(cmd);
+// 	vars = get_env_variables(env);
+
+// 	if (!path || access(path, X_OK) == -1)
+// 	{
+// 		if (is_directory(cmd->cmd))
+// 			print_cmd_error(cmd->cmd, "Is a directory");
+// 		else
+// 			print_cmd_error(cmd->cmd, "command not found");
+// 		return ((path == NULL) ? 127 : 126);
+// 	}
+// 	execve(path, arg, vars);
+// 	if (is_directory(path))
+// 		print_cmd_error(path, "Is a directory");
+// 	else
+// 		print_cmd_error(cmd->cmd, "command not found");
+// 	return (127);
+// }
+
+
 int	execve_bin(t_cmd *cmd, t_env *env)
 {
 	char	*path;
 	char	**arg;
 	char	**vars;
+	int		re;
 
+	re = 0;
 	path = get_path(cmd, env);
 	arg = get_args(cmd);
 	vars = get_env_variables(env);
-
 	if (!path || access(path, X_OK) == -1)
 	{
 		if (is_directory(cmd->cmd))
 			print_cmd_error(cmd->cmd, "Is a directory");
 		else
 			print_cmd_error(cmd->cmd, "command not found");
-		return ((path == NULL) ? 127 : 126);
+		if (path == NULL)
+			return (127);
+		else
+			return (126);
 	}
-	execve(path, arg, vars);
-	if (is_directory(path))
-		print_cmd_error(path, "Is a directory");
+	if (check_cmd_standard(cmd))
+	{
+		if (access(path, X_OK) == -1) 
+		{
+    		perror("Permission denied or command not executable");
+    		return (2);
+		}
+		execve(path, arg, vars);
+		perror("Command execution failed");
+		re = 1;
+	}
 	else
-		print_cmd_error(cmd->cmd, "command not found");
-	return (127);
+	{
+		apply_red(cmd, env);
+		execve(path, arg, vars);
+		// perror("Command execution failed");
+		if (is_directory(path))
+			print_cmd_error(path, "Is a directory");
+		else
+			print_cmd_error(cmd->cmd, "command not found");
+		re = 1;
+	}
+	return (re);
 }
-
-
-// int	execve_bin(t_cmd *cmd, t_env *env)
-// {
-// 	char	*path;
-// 	char	**arg;
-// 	char	**vars;
-// 	int		re;
-
-// 	re = 0;
-// 	path = get_path(cmd, env);
-// 	arg = get_args(cmd);
-// 	vars = get_env_variables(env);
-// 	if (check_cmd_standard(cmd))
-// 	{
-// 		if (access(path, X_OK) == -1) 
-// 		{
-//     		perror("Permission denied or command not executable");
-//     		return (2);
-// 		}
-// 		execve(path, arg, vars);
-// 		perror("Command execution failed");
-// 		re = 1;
-// 	}
-// 	else
-// 	{
-// 		execve(path, arg, vars);
-// 		perror("Command execution failed");
-// 		re = 1;
-// 	}
-// 	return (re);
-// }
